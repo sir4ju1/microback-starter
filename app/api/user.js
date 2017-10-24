83bfc00bb5d22ae42afe1c00295de466fc95db19
Sirajul <rianaville@gmail.com>
2017-10-24 08:41:40 +0600

    add image upload using sharp

--- app/api/user.js
+++ app/api/user.js
@@ -1,5 +1,11 @@
 import { RestGen, route, noauth, auth } from 'microback'
 import User from 'model/user'
+import sharp from 'sharp'
+import path from 'path'
+import uuid from 'uuid'
+import { config } from 'dotenv'
+
+config()
 
 class UserApi extends RestGen {
   constructor () {
@@ -24,11 +30,26 @@ class UserApi extends RestGen {
     const token = await auth.login(body, User)
     ctx.body = { success: true, data: token }
   }
+  /**
+   * Generally we save uploaded image to a location which is serveed
+   * by fast static content delivery server e.g. ngninx, GWAN etc
+   */
   @route('post', 'image/upload')
   @noauth()
   async upload (ctx) {
     const body = ctx.request.body
-    console.log(body)
+    const imagePath = body.files.image.path
+    const imageUniqueName = uuid()
+    // Generate Thumbnail of uploaded Image
+    await sharp(imagePath)
+      .resize(100)
+      .png()
+      .toFile(path.join(process.env.IMAGEPATH, `${imageUniqueName}_sm.png`))
+    // Generate 500px of uploaded Image
+    await sharp(imagePath)
+      .resize(500)
+      .png()
+      .toFile(path.join(process.env.IMAGEPATH, `${imageUniqueName}_lg.png`))
     ctx.body = { success: true }
   }
 }
